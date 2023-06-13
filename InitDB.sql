1fe472a0ef53c81d1ba9db2dbc2aec737312f4da
Damián Clancig <damian@clancig.com.ar>
2023-06-12 21:37:25 -0300

    Se agrega inserts para inicializar datos

--- InitDB.sql
+++ InitDB.sql
@@ -29,3 +29,34 @@ create table usuario
 	nombre varchar(20),
 	primary key (id_usuario)
 );
+
+select * from categoria
+
+insert into categoria (id_categoria, nombre) values (1, 'terror');
+insert into categoria (id_categoria, nombre) values (2, 'suspenso');
+insert into categoria (id_categoria, nombre) values (3, 'acción');
+
+
+select * from usuario
+
+insert into usuario (id_usuario, nombre) values (1, 'Damian');
+insert into usuario (id_usuario, nombre) values (2, 'Mayra');
+insert into usuario (id_usuario, nombre) values (3, 'Onix');
+
+
+select * from autor
+
+insert into autor (id_autor, nombre) values (1, 'stephen');
+insert into autor (id_autor, nombre) values (2, 'stallone');
+insert into autor (id_autor, nombre) values (3, 'otroAutor');
+
+
+select * from libro;
+
+insert into libro (id_libro, nombre, autor_pkey, categoria_pkey, usuario_pkey) values
+(1, 'avatar', 1, 3, 2);
+insert into libro (id_libro, nombre, autor_pkey, categoria_pkey, usuario_pkey) values
+(2, 'terminantor', 2, 1, 3);
+insert into libro (id_libro, nombre, autor_pkey, categoria_pkey, usuario_pkey) values
+(3, 'algun libro', 3, 2, 1);
+
